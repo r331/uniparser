f6d13cb5f819b316e53e65af3fe474cb52052744
Roman Ivanov <roivanoff@gmail.com>
2020-04-07 17:57:35 +0300

    add csv

---
+++ src/test/java/dev/ivanov/parser/ParserTest.java
@@ -0,0 +1,14 @@
+package dev.ivanov.parser;
+
+import org.junit.Test;
+
+public class ParserTest {
+    @Test
+    public void testCSV() {
+    }
+
+    @Test
+    public void testXLS() {
+    }
+
+}
